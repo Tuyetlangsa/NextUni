4380a71270b832a8b4aaab11c1e99559b943fd6f
Luong Hong My <mylhse184354@fpt.edu.vn>
2025-06-23 22:40:13 +0700

    Add Hide and Unhide University

---
+++ src/Modules/Academic/NextUni.Modules.Academic.Api/Universities/HideUniversity.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using NextUni.Common.Api.Endpoints;
+using NextUni.Common.Api.Results;
+using NextUni.Common.Domain;
+
+namespace NextUni.Modules.Academic.Api.Universities
+{
+    internal sealed class HideUniversity : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapPut("universities/status/{id}", async ([FromRoute] Guid id, ISender sender) =>
+            {
+                Result<Guid> result = await sender.Send(
+                    new Application.Universities.HideUniversity.HideUniversity.Command(id));
+
+                return result.MatchCreated(id => $"/universities/status/{id}");
+            })
+                .AllowAnonymous()
+                .WithTags(Tags.Academic);
+        }
+    }
+}
