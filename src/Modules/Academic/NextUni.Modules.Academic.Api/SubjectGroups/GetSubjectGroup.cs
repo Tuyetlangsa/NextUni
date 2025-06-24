34a4fa591459232c80b0e73a8307e1206038978a
Hoang Long Duong <longheo242@gmail.com>
2025-06-24 15:30:09 +0700

    Feat: add GetSubjectGroups

---
+++ src/Modules/Academic/NextUni.Modules.Academic.Api/SubjectGroups/GetSubjectGroup.cs
@@ -0,0 +1,42 @@
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using NextUni.Common.Api.Endpoints;
+using NextUni.Common.Api.Results;
+
+namespace NextUni.Modules.Academic.Api.SubjectGroups;
+
+public class GetSubjectGroups : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("subject-groups", async ([FromQuery] int pageNumber, [FromQuery] int pageSize, ISender sender) =>
+            {
+                var result =
+                    await sender.Send(
+                        new Application.SubjectGroups.GetSubjectGroups.GetSubjectGroups.Query(
+                            pageNumber, 
+                            pageSize,
+                            false));
+                return result.MatchOk();
+            })
+            .AllowAnonymous()
+            .WithTags(Tags.Academic);
+
+        app.MapGet("admin/subject-groups", async ([FromQuery] int pageNumber, [FromQuery] int pageSize, ISender sender) =>
+            {
+                var result =
+                    await sender.Send(
+                        new Application.SubjectGroups.GetSubjectGroups.GetSubjectGroups.Query(
+                            pageNumber, 
+                            pageSize,
+                            true));
+                return result.MatchOk();
+            })
+            .AllowAnonymous()
+            .WithTags(Tags.Academic);
+    }
+}
+
