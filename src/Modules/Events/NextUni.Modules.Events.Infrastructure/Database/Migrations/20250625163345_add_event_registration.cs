0243036a9185f7b30cb02b2da06b91b61577de18
Hoang Long Duong <longheo242@gmail.com>
2025-06-25 23:33:57 +0700

    add migration

---
+++ src/Modules/Events/NextUni.Modules.Events.Infrastructure/Database/Migrations/20250625163345_add_event_registration.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NextUni.Modules.Events.Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_event_registration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "event_registrations",
+                schema: "events",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    event_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    user_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    status = table.Column<bool>(type: "boolean", nullable: false, defaultValue: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_event_registrations", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "event_registrations",
+                schema: "events");
+        }
+    }
+}
