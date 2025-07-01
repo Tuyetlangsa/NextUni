185146ebafb166cb6283dda5ee1ac01da5388f5c
Hoang Long Duong <longheo242@gmail.com>
2025-07-01 21:57:26 +0700

    Fix: add migration initial module chatbot

---
+++ src/Modules/Chatbot/NextUni.Modules.Chatbot.Infrastructure/Database/Migrations/20250701145453_Intitial_Chatbot_Module_Migration.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NextUni.Modules.Chatbot.Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class Intitial_Chatbot_Module_Migration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "chatbot");
+
+            migrationBuilder.CreateTable(
+                name: "inbox_message_consumers",
+                schema: "chatbot",
+                columns: table => new
+                {
+                    inbox_message_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_inbox_message_consumers", x => new { x.inbox_message_id, x.name });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "inbox_messages",
+                schema: "chatbot",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    type = table.Column<string>(type: "text", nullable: false),
+                    content = table.Column<string>(type: "jsonb", maxLength: 2000, nullable: false),
+                    occurred_on_utc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    processed_on_utc = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    error = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_inbox_messages", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "outbox_message_consumers",
+                schema: "chatbot",
+                columns: table => new
+                {
+                    outbox_message_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_outbox_message_consumers", x => new { x.outbox_message_id, x.name });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "outbox_messages",
+                schema: "chatbot",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    type = table.Column<string>(type: "text", nullable: false),
+                    content = table.Column<string>(type: "jsonb", maxLength: 2000, nullable: false),
+                    occurred_on_utc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    processed_on_utc = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    error = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_outbox_messages", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "semantic_embeddings",
+                schema: "chatbot",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    entity_type = table.Column<string>(type: "text", nullable: false),
+                    entity_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    embedding = table.Column<float[]>(type: "real[]", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_semantic_embeddings", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "inbox_message_consumers",
+                schema: "chatbot");
+
+            migrationBuilder.DropTable(
+                name: "inbox_messages",
+                schema: "chatbot");
+
+            migrationBuilder.DropTable(
+                name: "outbox_message_consumers",
+                schema: "chatbot");
+
+            migrationBuilder.DropTable(
+                name: "outbox_messages",
+                schema: "chatbot");
+
+            migrationBuilder.DropTable(
+                name: "semantic_embeddings",
+                schema: "chatbot");
+        }
+    }
+}
