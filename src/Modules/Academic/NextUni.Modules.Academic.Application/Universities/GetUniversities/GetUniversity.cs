77e39f3a4646241c6c64f2dfa2e7c306dc240fb9
Luong Hong My <mylhse184354@fpt.edu.vn>
2025-07-01 22:02:21 +0700

    Fix paging Get University

--- src/Modules/Academic/NextUni.Modules.Academic.Application/Universities/GetUniversities/GetUniversity.cs
+++ src/Modules/Academic/NextUni.Modules.Academic.Application/Universities/GetUniversities/GetUniversity.cs
@@ -41,8 +41,8 @@ public abstract class GetUniversity
                 QueryFilter.None => query
             };
 
-            
-            var count = await dbContext.Universities.CountAsync(cancellationToken);
+
+            var count = await query.CountAsync(cancellationToken);
 
             var responses = await query.Applypagination(request.PageNumber, request.PageSize).Select(university => new ResponseItem(
                 university.Id, 
